b39b24c2de0b8da01128c5040347f18d8b55f2f1
saiprasanth424 <saiprasanth424@gmail.com>
2020-07-04 16:47:54 +0530

    First Git Push

---
+++ ingestion/src/main/java/com/casestudy/model/FacebookData.java
@@ -0,0 +1,48 @@
+package com.casestudy.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "facebook_data")
+public class FacebookData {
+	 
+	@Id
+	  @GeneratedValue(strategy=GenerationType.AUTO)
+	  private Integer id;
+	 private String name;
+	 private String picture;
+	 private String profile_name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getProfile_name() {
+		return profile_name;
+	}
+	public void setProfile_name(String profile_name) {
+		this.profile_name = profile_name;
+	}
+	
+	
+
+}
---
+++ ingestion/src/main/java/com/casestudy/model/FacebookRepository.java
@@ -0,0 +1,7 @@
+package com.casestudy.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface  FacebookRepository extends CrudRepository<FacebookData, Integer> {
+
+}
